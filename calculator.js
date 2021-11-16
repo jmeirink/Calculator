404a4b8ca8b9ef10e747fa679cee0a862515f054
jmeirink <84925553+jmeirink@users.noreply.github.com>
2021-11-16 08:21:47 -0600

    Added bodyParser package and added functionality to the calculator app

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/', function(req, res){
+  res.send("Thanks for posting that.")
+});
+
+console.log(req.body);
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
